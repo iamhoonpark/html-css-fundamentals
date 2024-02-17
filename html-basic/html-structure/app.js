6fd13cd621bf1e275ef3d0d6a67038188d5922b1
박상훈 <psh995666@gmail.com>
2024-02-18 01:28:27 +0900

    html 기본 구조 및 독타입

---
+++ html-basic/html-structure/app.js
@@ -0,0 +1,15 @@
+const heading = document.querySelector("h1");
+const audio = new Audio("./assets/audios/pop.wav");
+
+heading.addEventListener("click", function(event) {
+    audio.play();
+    if (this.active) {
+        this.style.color = "#000";
+    } else {
+        this.style.color = "#fff";
+    };
+
+    this.active = !this.active;
+})
+
+heading.active = true;
